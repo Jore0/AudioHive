e7a5b0ca035d663336c2a073e5584f420dec42e2
Jose Orea <jose.orea.15@gmail.com>
2019-10-01 21:54:28 -0400

    Added Components app and root

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+    <div>
+        <h1>AudioHive from the app.jsx</h1>
+    </div>
+);
+
+export default App;
--- frontend/soundCloud.jsx
+++ frontend/soundCloud.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store"
-//testing 
-import {login} from "./util/session_util_api_util"
-import {logout} from "./util/session_util_api_util"
-import {signup} from "./util/session_util_api_util"
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
-    window.login = login;
-    window.logout = logout;
-    window.signup = signup;
+
     const store = configureStore();
     window.store = store;
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to AudioHive</h1>, root);
+    ReactDOM.render(<Root store={store}/>, root);
 });
