1e35fb9a6f93a5d7a40eea9edcf61035089b5285
Jose Orea <jose.orea.15@gmail.com>
2019-10-10 14:30:47 -0400

    Comleted show page status bar

--- frontend/components/waveform/waveform.jsx
+++ frontend/components/waveform/waveform.jsx
@@ -7,17 +7,21 @@ class WaveForm extends React.Component {
         super(props)
         // debugger
         this.state = {
-            readyToPlay: false
+            readyToPlay: false, 
+            duration: this.props.song.duration || 0
         }
         this.waveform = React.createRef()
+        this.seekBar = React.createRef()
         this.wavesurfer = null
+        // this.updateTime = this.updateTime.bind(this)
         this.showWaveSurfer = this.showWaveSurfer.bind(this)
         this.StartPlay = this.StartPlay.bind(this)
     }
     
     componentDidMount(){
         this.props.fetchSong(this.props.song.id)
-        this.showWaveSurfer()
+        this.showWaveSurfer();
+        // this.updateTime();
     }
 
     // componentDidUpdate(){
@@ -50,27 +54,40 @@ class WaveForm extends React.Component {
             this.setState({readyToPlay: true })
         });
     }
+    secondsToMinutes(time) {
+        // debugger
+        return Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2)
+    }
 
     render(){
+       let maxy;
+       let currentTime;
+       let totalLength;
         if (this.wavesurfer){
             debugger 
             this.wavesurfer.seekTo(this.props.currentTime)
-        }
+            currentTime = this.secondsToMinutes(this.props.currentTime * this.wavesurfer.getDuration())
+            maxy = this.wavesurfer.getDuration()
+            totalLength = this.secondsToMinutes(this.wavesurfer.getDuration())
+            this.seekBar.current.value = this.props.currentTime * this.wavesurfer.getDuration();
+        }   
+        
         return (<>
+            <input className="blackSeekbar" ref={this.seekBar} type="range" min="0" max={maxy}/>
             <div className="wave-form-container">
-            <div className="wave" ref={this.waveform} ></div>
+
+            <div className="wave-form-times">
+                    <p className="wave-form-orange">{currentTime}</p>
+                    <p className="wave-form-transparent">{totalLength}</p>
             </div>
-            {/* <button onClick={this.StartPlay}>
-                Play
-            </button> */}
+            <div className="wave" ref={this.waveform}></div>
+
+            </div>
                 </>
                 )
             }
 
 }
-
-// export default WaveForm
-
 
 const msp = (state, ownProps) => {
     // debugger
@@ -83,7 +100,6 @@ const msp = (state, ownProps) => {
 }
 
 const mdp = dispatch => ({
-    // fetchSong: (id) => dispatch(fetchSong(id)),
     receiveCurrentSong: (song) => dispatch(receiveCurrentSong(song))
 })
 
